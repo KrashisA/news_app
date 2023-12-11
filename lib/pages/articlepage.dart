73cce08f05504895b663fd42aa603fe2fba00d8a
Krashis Adhikari <krashisadhikari@gmail.com>
2023-12-11 19:21:05 +0545

    Overflow Fixed

--- lib/pages/articlepage.dart
+++ lib/pages/articlepage.dart
@@ -37,82 +37,83 @@ class _NewsPageState extends State<NewsPage> {
                       return ListView.builder(
                           itemCount: snapshot.data!.articles!.length,
                           itemBuilder: (context, index) {
-                            return Padding(
-                                //card ui
-                                padding:
-                                    const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                                child: Stack(children: <Widget>[
-                                  Container(
-                                    padding: EdgeInsets.all(12),
-                                    width: MediaQuery.of(context).size.width *
-                                        0.95,
-                                    height: MediaQuery.of(context).size.height *
-                                        0.45,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(12),
-                                      color: Colors.grey.shade200,
-                                    ),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Image(
-                                            height: 180,
-                                            image: NetworkImage(snapshot.data!
-                                                .articles![index].urlToImage
-                                                .toString())),
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              4, 8, 4, 4),
-                                          child: Text(
-                                            snapshot
-                                                .data!.articles![index].title
-                                                .toString(),
-                                            style: TextStyle(
-                                                overflow: TextOverflow.clip,
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          height: 4,
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              4, 4, 4, 4),
-                                          child: Text(
-                                            snapshot.data!.articles![index]
-                                                .description
-                                                .toString(),
-                                            maxLines: 4,
-                                            style: TextStyle(
-                                              fontSize: 12,
-                                              overflow: TextOverflow.clip,
+                            return Container(
+                              child: Padding(
+                                  //card ui
+                                  padding:
+                                      const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                  child: Stack(children: <Widget>[
+                                    Container(
+                                      padding: EdgeInsets.all(12),
+                                      width: MediaQuery.of(context).size.width *
+                                          0.95,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.45,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: Colors.grey.shade200,
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Image(
+                                              height: 180,
+                                              image: NetworkImage(snapshot.data!
+                                                  .articles![index].urlToImage
+                                                  .toString())),
+                                          Padding(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                4, 8, 4, 4),
+                                            child: Text(
+                                              snapshot
+                                                  .data!.articles![index].title
+                                                  .toString(),
+                                              style: TextStyle(
+                                                  overflow: TextOverflow.clip,
+                                                  fontSize: 16,
+                                                  height: 1,
+                                                  fontWeight: FontWeight.bold),
                                             ),
                                           ),
-                                        ),
-                                      ],
+                                          Padding(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                4, 4, 4, 4),
+                                            child: Text(
+                                              snapshot.data!.articles![index]
+                                                  .description
+                                                  .toString(),
+                                              maxLines: 4,
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                overflow: TextOverflow.clip,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                  Positioned(
-                                      bottom: 15,
-                                      right:
-                                          15, //give the values according to your requirement
-                                      child: IconButton(
-                                        icon: Icon(
-                                          favArticle.contains(index)
-                                              ? Icons.favorite
-                                              : Icons.favorite_outline,
-                                          size: 36,
-                                        ),
-                                        onPressed: () {
-                                          favArticle.add(index);
-                                          setState(() {});
-                                        },
-                                      )),
-                                ]));
+                                    Positioned(
+                                        bottom: 4,
+                                        right:
+                                            6, //give the values according to your requirement
+                                        child: IconButton(
+                                          icon: Icon(
+                                            favArticle.contains(index)
+                                                ? Icons.favorite
+                                                : Icons.favorite_outline,
+                                            size: 36,
+                                          ),
+                                          onPressed: () {
+                                            favArticle.add(index);
+                                            setState(() {});
+                                          },
+                                        )),
+                                  ])),
+                            );
                           });
                     }
                   }))
